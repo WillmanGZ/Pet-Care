a76005c41f5163cdd2cc73ef0572d80fc5d44e0f
WillmanGZ <willmang@uninorte.edu.co>
2025-07-10 11:23:37 -0500

    feat: Add JSON server dependency and update login functionality

--- src/js/login.js
+++ src/js/login.js
@@ -5,3 +5,16 @@ const loginForm = document.getElementById("login-form");
 const usernameInput = document.getElementById("login-username");
 const passwordInput = document.getElementById("login-password");
 
+//API URL
+const API_URL = ''
+
+//Events
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  //Take current input values
+  const username = usernameInput.value.trim().toLowerCase();
+  const password = passwordInput.value;
+
+  let response = await fetch()
+});
